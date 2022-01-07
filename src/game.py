4a02d44d5dd079b418cb243ca578c61510923a59
^-^ Trevor <wasandland@outlook.com>
2022-01-07 17:42:40 +0000

    bobirt 4 life

---
+++ src/game.py
@@ -0,0 +1,12 @@
+class Game():
+    def __init__(self):
+        print("bobirty is love")
+
+    def update(self):
+        print("bobirty is life")
+
+    def events(self):
+        print("bobirty is king")
+
+    def draw(self):
+        print("all hail bobirty")
