2e52816d1c226fab1c1baa60e51da60d934b5a55
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-08-08 16:11:21 -0400

    removing unrendered chunks code

--- src/entities/player.py
+++ src/entities/player.py
@@ -13,34 +13,34 @@ if TYPE_CHECKING: # Type annotations without causing circular imports
 
 import pygame
 
-from src.utils.constants import VEC, GRAVITY, BLOCK_SIZE, SCR_DIM
+from src.utils.constants import CHUNK_SIZE, VEC, GRAVITY, BLOCK_SIZE, SCR_DIM
 from src.management.sprite import Sprite, LayersEnum
 from src.entities.entity import Entity
 from src.utils.utils import sign
 
 from pygame.locals import (
-    K_a, K_d, K_w, K_LEFT, K_RIGHT, K_UP, K_SPACE
+    K_a, K_d, K_w, K_s, K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE
 )
 
 class Camera:
     """Class that represents the camera"""
-    def __init__(self, player) -> None:
+    def __init__(self, manager: GameManager, player) -> None:
+        self.manager = manager
+        self.scene = self.manager.scene
         self.player = player
-        self.pos = self.player.size / 2
-        self.pos = self.player.pos - self.pos - VEC(SCR_DIM) / 2 + self.player.size / 2
+        self.pos = self.player.pos - VEC(SCR_DIM) / 2
+        self.coords = (self.pos + (VEC(SCR_DIM) / 2 - self.player.size / 2)) // BLOCK_SIZE
+        self.chunk_coords = self.coords // CHUNK_SIZE
         self.scalar = VEC(0.6, 1.2)
 
-    def update(self, dt: float) -> None:
+    def update(self) -> None:
         mpos = pygame.mouse.get_pos()
 
         # How much the player moved since last frame (relative to camera)
         tick_offset = self.player.pos - self.pos - (VEC(SCR_DIM) / 2 - self.player.size / 2)
 
         # Calculate the distance the mouse is from the center of the screen
-        if not self.player.inventory.visible:
-            x_dist, y_dist = mpos[0] - SCR_DIM[0] / 2, mpos[1] - SCR_DIM[1] / 2
-        else: # (if the player is in their inventory)
-            x_dist, y_dist = 0, 0
+        x_dist, y_dist = mpos[0] - SCR_DIM[0] / 2, mpos[1] - SCR_DIM[1] / 2
 
         # Squaring the distance so that the further out the cursor from the center, the camera moves exponentially away
         # Times a scalar so that the camera can move further in the y-direction but less in the x-direction
@@ -50,14 +50,16 @@ class Camera:
         )
 
         # Combined the offset due to player movement and offset due to cursor, and scale it down to a reasonable value
-        self.pos += (tick_offset * 2 + VEC(dist_squared) / 300) * dt
+        self.pos += (tick_offset * 2 + VEC(dist_squared) / 300) * self.manager.dt
+        self.coords = (self.pos + (VEC(SCR_DIM) / 2 - self.player.size / 2)) // BLOCK_SIZE
+        self.chunk_coords = self.coords // CHUNK_SIZE
 
 class Player(Entity):
     """A sprite that represents the game's player """
 
     def __init__(self, manager: GameManager) -> None:
         super().__init__(VEC(0, 3) * BLOCK_SIZE, VEC(0.225 * BLOCK_SIZE, 1.8 * BLOCK_SIZE), manager, LayersEnum.PLAYER)
-        self.camera = Camera(self)
+        self.camera = Camera(self.manager, self)
         self.slide = 1200 # Speed of acceleration and deceleration
         self.speed = 250 # Max speed
         self.jump_vel = -800 # Velocity at start of jump
@@ -73,10 +75,20 @@ class Player(Entity):
             self.acc.x += self.slide # Accelerate
         elif self.vel.x > 0:
             self.acc.x -= self.slide # Decelerate
+        if keys[K_UP] or keys[K_w]:
+            self.acc.y -= self.slide # Accelerate
+        elif self.vel.y < 0:
+            self.acc.y += self.slide # Decelerate
+        if keys[K_DOWN] or keys[K_s]:
+            self.acc.y += self.slide # Accelerate
+        elif self.vel.y > 0:
+            self.acc.y -= self.slide # Decelerate
         # if (keys[K_UP] or keys[K_w] or keys[K_SPACE]) and self.on_ground:
         #     self.vel.y = self.jump_vel
 
         super().update()
+        
+        self.camera.update()
 
     def draw(self) -> None:
         pygame.draw.rect(self.manager.screen, (255, 0, 0), (*(self.pos - self.camera.pos), *self.size))
--- src/world/chunk.py
+++ src/world/chunk.py
@@ -15,7 +15,7 @@ from pygame.locals import SRCALPHA
 from random import seed, choice
 import pygame
 
-from src.utils.constants import VEC, CHUNK_SIZE, WIDTH, HEIGHT, CHUNK_PIXEL_SIZE, BLOCK_SIZE
+from src.utils.constants import VEC, CHUNK_SIZE, WIDTH, HEIGHT, CHUNK_PIXEL_SIZE, BLOCK_SIZE, SCR_DIM
 from src.management.sprite import Sprite, LayersEnum
 from src.utils.utils import pairing, PosDict
 from src.world.block import Location
@@ -40,11 +40,14 @@ class ChunkData(PosDict):
             self[pos] = Location(self.manager, self.master, pos, *generate_location(pos))
 
 class Chunk(Sprite):
-    def __init__(self, manager: GameManager, chunk_pos: tuple[int, int] | VEC) -> None:
+    def __init__(self, manager: GameManager, chunk_pos: tuple[int, int] | VEC, chunk_data=None) -> None:
         super().__init__(manager, LayersEnum.BLOCKS)
         self.chunk_manager = self.scene.chunk_manager
         self.chunk_pos = VEC(chunk_pos)
-        self.chunk_data = ChunkData(self.manager, self, self.chunk_pos)
+        if not chunk_data:
+            self.chunk_data = ChunkData(self.manager, self, self.chunk_pos)
+        else:
+            self.chunk_data = chunk_data
         self.image = pygame.Surface((CHUNK_PIXEL_SIZE, CHUNK_PIXEL_SIZE), SRCALPHA)
         for coords, location in self.chunk_data.items():
             self.update_image(VEC(coords), location.image)
@@ -56,22 +59,36 @@ class Chunk(Sprite):
 
     def draw(self):
         self.manager.screen.blit(self.image, self.chunk_pos * CHUNK_PIXEL_SIZE - self.scene.player.camera.pos)
-        # pygame.draw.rect(self.manager.screen, (255, 255, 0), (*(self.chunk_pos * CHUNK_PIXEL_SIZE - self.scene.player.camera.pos), CHUNK_PIXEL_SIZE, CHUNK_PIXEL_SIZE), 2)
+        pygame.draw.rect(self.manager.screen, (255, 255, 0), (*(self.chunk_pos * CHUNK_PIXEL_SIZE - self.scene.player.camera.pos), CHUNK_PIXEL_SIZE, CHUNK_PIXEL_SIZE), 2)
 
 class ChunkManager:
     def __init__(self, manager):
         self.manager = manager
         self.scene = self.manager.scene
-        self.chunks: dict[tuple[int, int], Chunk] = PosDict()
-        self.displayed_chunks = VEC(WIDTH // CHUNK_PIXEL_SIZE + 2, HEIGHT // CHUNK_PIXEL_SIZE + 2)
-        self.topleft_chunk_pos = self.scene.player.chunk_coords - self.displayed_chunks // 2 # +- 1
+        self.chunks = PosDict()
+        self.old_chunk_data = PosDict()
+        self.displayed_chunks = VEC(WIDTH // CHUNK_PIXEL_SIZE + 1, HEIGHT // CHUNK_PIXEL_SIZE + 1)
+        self.topleft_chunk_pos = self.scene.player.chunk_coords - self.displayed_chunks // 2 - (1, 0)
         self.bottomright_chunk_pos = self.topleft_chunk_pos + self.displayed_chunks
 
     def update(self) -> None:
+        self.topleft_chunk_pos = self.scene.player.camera.chunk_coords - self.displayed_chunks // 2 - (1, 0)
+        self.bottomright_chunk_pos = self.topleft_chunk_pos + self.displayed_chunks
+        new_chunks = PosDict()
         for y in range(int(self.topleft_chunk_pos.y), int(self.bottomright_chunk_pos.y + 1)):
             for x in range(int(self.topleft_chunk_pos.x), int(self.bottomright_chunk_pos.x + 1)):
-                if (x, y) not in self.chunks:
-                    self.chunks[(x, y)] = Chunk(self.manager, (x, y))
+                if (x, y) not in self.chunks: # If the chunk is inactive or ungenerated
+                    # If it has been generated before and stored in old_chunk_data, use the chunk data from there
+                    new_chunks[(x, y)] = Chunk(self.manager, (x, y), self.old_chunk_data[(x, y)] if (x, y) in self.old_chunk_data else None)
+                else: # If the chunk is currently active
+                    # It is in the new chunks, also in the current chunks, thus just copy the object over to new chunks
+                    new_chunks[(x, y)] = self.chunks[(x, y)]
+                    # Delete from current chunks, so that what's left can be stored in old_chunk_data below
+                    del self.chunks[(x, y)]
+        # Store the chunk data of the remaining chunks in old_chunk_data
+        for chunk_pos, chunk in self.chunks.items():
+            self.old_chunk_data[chunk_pos] = chunk.chunk_data
+        self.chunks = new_chunks
 
 def generate_location(coords):
     coords = VEC(coords)
