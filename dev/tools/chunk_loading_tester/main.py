12caa31b08310bbdd27b7333641cfd58d03c0ffd
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-08-19 22:04:38 -0400

    ULTRA FAST PERLIN NOISE :DDDDDDD

--- dev/tools/chunk_loading_tester/main.py
+++ dev/tools/chunk_loading_tester/main.py
@@ -3,6 +3,7 @@ import pygame
 from random import *
 from pygame.locals import *
 from typing import Generator
+from pyfastnoisesimd import Noise
 from pygame.math import Vector2 as VEC
 
 from my_profile import profile
@@ -15,7 +16,10 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | HWSURFACE)
 clock = pygame.time.Clock()
 
-def generate_location(coords: tuple[int, int]) -> tuple[str | None, str | None, str | None]:
+noise = Noise(SEED, 8)
+noise.fractal.lacunarity = 0.8
+
+def generate_location(coords: tuple[int, int], cave: bool) -> tuple[str | None, str | None, str | None]:
     """Generate the names of the blocks to go at a certain location. Temporary world gen!"""
 
     coords = VEC(coords)
@@ -27,7 +31,7 @@ def generate_location(coords: tuple[int, int]) -> tuple[str | None, str | None, 
         name = "stone"
     else:
         name = None
-    return (name, name) # Very temporary - we want all slices to be the same
+    return (name,) if cave else (name, name)
 
 class ChunkData(PosDict):
     """Custom dictionary that also has methods that handle chunk generation"""
@@ -37,16 +41,17 @@ class ChunkData(PosDict):
         self.generate_base()
 
     def iterate(self) -> Generator:
-        """Iterates through every coordinate inside the chunk, yields the absolute coordinates, NOT chunk coordinates"""
+        """Iterates through every coordinate inside the chunk, yields the absolute coordinates AND chunk coordinates"""
         for y in range(int(self.chunk_pos.y * CHUNK_SIZE), int(self.chunk_pos.y * CHUNK_SIZE + CHUNK_SIZE)):
             for x in range(int(self.chunk_pos.x * CHUNK_SIZE), int(self.chunk_pos.x * CHUNK_SIZE + CHUNK_SIZE)):
-                yield (x, y)
+                yield (x, y), inttup((x - self.chunk_pos.x * CHUNK_SIZE, y - self.chunk_pos.y * CHUNK_SIZE))
 
     def generate_base(self) -> None:
         """Generate every block in the chunk"""
         seed(pairing(3, *self.chunk_pos, SEED))
-        for pos in self.iterate():
-            self[pos] = Location(self.master, pos, *generate_location(pos))
+        noise_grid = noise.genAsGrid([8, 8, 1], [int(self.chunk_pos.x * CHUNK_SIZE), int(self.chunk_pos.y * CHUNK_SIZE), 0])
+        for pos, in_chunk_pos in self.iterate():
+            self[pos] = Location(self.master, pos, *generate_location(pos, 0.145 < noise_grid[in_chunk_pos[0]][in_chunk_pos[1]][0] < 0.275))
 
 class Chunk:
     instances = PosDict()
@@ -71,7 +76,11 @@ class Chunk:
 
     def draw(self):
         screen.blit(self.image, self.chunk_pos * CHUNK_PIXEL_SIZE)
-        pygame.draw.rect(screen, (255, 255, 0), (self.chunk_pos * CHUNK_PIXEL_SIZE, self.image.get_size()), 1)
+        # pygame.draw.rect(screen, (255, 255, 0), (self.chunk_pos * CHUNK_PIXEL_SIZE, self.image.get_size()), 1)
+
+for x in range(WIDTH // CHUNK_PIXEL_SIZE + 1):
+    for y in range(HEIGHT // CHUNK_PIXEL_SIZE + 1):
+        Chunk((x, y))
 
 running = True
 while running:
