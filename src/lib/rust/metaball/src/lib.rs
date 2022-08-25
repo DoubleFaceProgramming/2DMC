08381815b07923cbe66361d6ec6ec5dbd71bacd1
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-08-25 11:01:56 -0400

    moar blob gen optimization

--- src/lib/rust/metaball/src/lib.rs
+++ src/lib/rust/metaball/src/lib.rs
@@ -28,28 +28,33 @@ impl MetaBall {
 }
 
 #[pyfunction]
-fn blob(seed: i64) -> PyResult<Vec<(i32, i32)>> {
+fn blob(seed: i64, size: i32, num_balls: (i32, i32), inner_range: (i32, i32), ball_size: (i32, i32)) -> PyResult<Vec<(i32, i32)>> {
     let mut rng = ChaCha8Rng::seed_from_u64(if seed < 0 { (-seed * 2 - 1) as u64 } else { (seed * 2) as u64 });
 
     let mut balls = vec![];
-    for _ in 0..rng.gen_range(3..4 + 1) {
-        balls.push(MetaBall::new(rng.gen_range(4..11 + 1) as i32, rng.gen_range(4..11 + 1) as i32, rng.gen_range(2..4 + 1) as f32))
+    for _ in 0..rng.gen_range(num_balls.0..=num_balls.1) {
+        balls.push(MetaBall::new(
+            rng.gen_range(inner_range.0..=inner_range.1) as i32,
+            rng.gen_range(inner_range.0..=inner_range.1) as i32,
+            rng.gen_range(ball_size.0..=ball_size.1) as f32
+        ))
     }
 
     let mut coordinates = vec![];
-    for y in 0..16 {
-       for x in 0..16 {
+    for y in 0..size {
+       for x in 0..size {
             let mut val = 0.0;
             for ball in &balls {
                 let distance_squared = ball.center.dist_sqr(&Coord {x, y});
                 val += if distance_squared == 0.0 {
-                    10.0
+                    20.0
                 } else {
                     ball.radius * ball.radius / distance_squared / 1.6
                 };
             }
             if val > 1.2 {
-                coordinates.push((x, y))
+                coordinates.push((x, y));
+                break;
             }
         }
     }
--- src/lib/rust/metaball/test.py
+++ src/lib/rust/metaball/test.py
@@ -2,19 +2,26 @@ from random import randint
 import metaball
 import time
 
-trials = 100000
+trials = 1000000
 
+print(f"Preparing {trials} random seeds...")
+rand = [randint(0, 10000000) for _ in range(trials)]
+
+print(f"Generating {trials} blobs...")
 start = time.time()
-for _ in range(trials):
-    metaball.blob(100000)
-print(f"{((time.time() - start) / trials):.20f}")
+for i in range(trials):
+    metaball.blob(rand[i], 16, (3, 5), (4, 11), (2, 4))
+print()
+print(f"AVERAGE: {((time.time() - start) / trials):.8f}")
+print(f"TOTAL: {(time.time() - start):.8f}")
 
 # grid = [["." for _ in range(16)] for _ in range(16)]
-# for pos in metaball.blob(randint(0, 10000000)):
+# for pos in metaball.blob(randint(0, 10000000), 16, (3, 5), (4, 11), (2, 4)):
 #     grid[pos[1]][pos[0]] = "#"
 # for row in grid:
 #     for ch in row:
 #         print(ch, end=" ")
 #     print()
 
-# 0.00000477
+# 0.0000057
+# 0.0000022
